12c52f87e7a2818ed73fd8d503a5bba517f686dd
anto <anto@exordiumgames.com>
2024-05-15 07:25:16 +0200

    feature - mesh generation WIP (upside down)

---
+++ Assets/PTG3/Scripts/NoiseMapMeshGenerator.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseMapMeshGenerator
+{
+    public static Mesh GenerateTerrainMesh(float[,] heightMap, float heightMultiplier)
+    {
+        var width = heightMap.GetLength(0);
+        var height = heightMap.GetLength(1);
+
+        var topLeftX = (width - 1) / -2f;
+        var topLeftZ = (height - 1) / 2f;
+
+        var meshData = new NoiseMapMeshData(width, height);
+        var vertexIndex = 0;
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                meshData.Vertices[vertexIndex] = new Vector3(topLeftX + x, heightMap[x, y] * heightMultiplier, topLeftZ - y);
+                meshData.UVs[vertexIndex] = new Vector2(x / (float)width, y / (float)height);
+
+                if (x < width - 1 && y < height - 1)
+                {
+                    meshData.AddTriangle(vertexIndex, vertexIndex + width + 1, vertexIndex + width);
+                    meshData.AddTriangle(vertexIndex + width + 1, vertexIndex, vertexIndex + 1);
+                }
+
+                vertexIndex++;
+            }
+        }
+
+        return meshData.CreateMesh();
+    }
+}
+
+public class NoiseMapMeshData
+{
+    public Vector3[] Vertices;
+    public int[] Triangles;
+    public Vector2[] UVs;
+
+    private int _currentTriangleIndex;
+
+    public NoiseMapMeshData(int meshWidth, int meshHeight)
+    {
+        Vertices = new Vector3[meshWidth * meshHeight];
+        UVs = new Vector2[meshWidth * meshHeight];
+        Triangles = new int[(meshWidth - 1) * (meshHeight - 1) * 6];
+    }
+
+    public void AddTriangle(int a, int b, int c)
+    {
+        Triangles[_currentTriangleIndex] = a;
+        Triangles[_currentTriangleIndex + 1] = b;
+        Triangles[_currentTriangleIndex + 2] = c;
+
+        _currentTriangleIndex += 3;
+    }
+
+    public Mesh CreateMesh()
+    {
+        var mesh = new Mesh();
+
+        mesh.vertices = Vertices;
+        mesh.triangles = Triangles;
+        mesh.uv = UVs;
+
+        mesh.RecalculateNormals();
+
+        return mesh;
+    }
+}
--- Assets/PTG3/Scripts/NoiseMapRendererController.cs
+++ Assets/PTG3/Scripts/NoiseMapRendererController.cs
@@ -2,6 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum NoiseMapRenderType
+{
+    Plane,
+    Mesh
+}
+
 public class NoiseMapRendererController : MonoBehaviour
 {
     [Header("Data")]
@@ -9,24 +15,28 @@ public class NoiseMapRendererController : MonoBehaviour
     [SerializeField] private int _chunkHeight = 255;
     [SerializeField] private int _worldWidth = 1;
     [SerializeField] private int _worldHeight = 1;
-    [SerializeField] private float _worldScale = 1f;
-    [SerializeField] private bool _falloff = false;
+    [SerializeField] private float _worldScale = 4f;
+    [SerializeField] private bool _falloff = true;
     [Range(0f, 1f)]
     [SerializeField] private float _mainlandSize = 1f;
     [Range(1f, 80f)]
-    [SerializeField] private float _falloffTransitionWidth = 1f;
+    [SerializeField] private float _falloffTransitionWidth = 5.6f;
     [SerializeField] private AnimationCurve _heightMapHeightCurve;
     [SerializeField] private List<ColorMapHeightColorData> _heightColorData = new List<ColorMapHeightColorData>();
     [SerializeField] private bool _blackAndWhite = false;
     [Range(1,4)]
-    [SerializeField] private int _octaves = 1;
+    [SerializeField] private int _octaves = 2;
     [Range(0f, 1f)]
-    [SerializeField] private float _persistence = 1f;
+    [SerializeField] private float _persistence = 0.17f;
     [Range(0f, 5f)]
-    [SerializeField] private float _lacunarity = 1f;
+    [SerializeField] private float _lacunarity = 4.3f;
+    [SerializeField] private float _heightMultiplier = 1f;
+    [SerializeField] private NoiseMapRenderType _renderType = NoiseMapRenderType.Plane;
 
     [Header("Component References")]
     [SerializeField] private Renderer _renderer;
+    [SerializeField] private MeshRenderer _meshRenderer;
+    [SerializeField] private MeshFilter _meshFilter;
 
     private void SetMeshRendererTexture()
     {
@@ -38,8 +48,34 @@ public class NoiseMapRendererController : MonoBehaviour
         _renderer.transform.localScale = new Vector3(_worldWidth * _chunkWidth, 1f, _worldHeight * _chunkHeight);
     }
 
+    private void SetSingleMeshRendererTexture()
+    {
+        var worldData = NoiseMapGenerator.GeneratePerlinNoiseWorldHeightMap(_worldWidth, _worldHeight, _chunkWidth, _chunkHeight, _worldScale, _falloff, _mainlandSize, _falloffTransitionWidth, _heightMapHeightCurve, _octaves, _persistence, _lacunarity);
+        var colorMap = ColorMapGenerator.GenerateColorMapFromWorldHeightMap(worldData, _blackAndWhite, _heightColorData);
+        var texture = TextureGenerator.GenerateTextureFromColorMap(colorMap, _worldWidth * _chunkWidth, _worldHeight * _chunkHeight);
+
+        var mesh = NoiseMapMeshGenerator.GenerateTerrainMesh(worldData[Vector2Int.zero], _heightMultiplier);
+        mesh.RecalculateNormals();
+
+        _meshFilter.mesh = mesh;
+        _meshRenderer.sharedMaterial.mainTexture = texture;
+        //_meshRenderer.transform.localScale = new Vector3(_worldWidth * _chunkWidth, 1f, _worldHeight * _chunkHeight);
+    }
+
     private void OnValidate()
     {
-        SetMeshRendererTexture();
+        _renderer.gameObject.SetActive(false);
+        _meshRenderer.gameObject.SetActive(false);
+
+        if (_renderType == NoiseMapRenderType.Mesh && _worldHeight == 1 && _worldWidth == 1)
+        {
+            _meshRenderer.gameObject.SetActive(true);
+            SetSingleMeshRendererTexture();
+        }
+        else
+        {
+            _renderer.gameObject.SetActive(true);
+            SetMeshRendererTexture();
+        }
     }
 }
